7bb819561318167e587290ccb054ca34bab1dacc
Kasra Nadri <kasra.n2018@gmail.com>
2025-02-09 19:52:05 +0330

    created a full pipeline for the data pre-processing and tested the linear regression model performance and decisiontree for it

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from pandas.plotting import scatter_matrix
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
+from util.ClusterSimilarity import ClusterSimilarity
+from sklearn.compose import make_column_selector
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
 
 
 #print(housing.info())
@@ -58,6 +71,7 @@ corr_matrix = housing.drop('ocean_proximity', axis = 1).corr()
 housing = strat_train_set.drop("median_house_value", axis = 1)
 housing_labels = strat_train_set["median_house_value"].copy()
 
+
 #========== STEP 1: CLEANING ==========#
 imputer = SimpleImputer(strategy="median")
 
@@ -70,5 +84,100 @@ X = imputer.transform(housing_num)
 housing_tr = pd.DataFrame(X, columns=housing_num.columns, index = housing_num.index)
 
 #========== STEP 2: HANDLING CATEGORIAL ATTRIBUTES ==========#
-housing_cat = housing["ocean_proximity"]
-print(housing_cat.head(10))
+housing_cat = housing[["ocean_proximity"]]
+#print(housing_cat.head(10))
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot = housing_cat_1hot.toarray()
+
+
+#========== STEP 3: FEATURE SCALING ==========#
+min_max_scaler = MinMaxScaler(feature_range= (-1, 1))
+housing_num_min_max_scaled = min_max_scaler.fit_transform(housing_num)
+
+
+std_scaler = StandardScaler()
+housing_num_std_scaled = std_scaler.fit_transform(housing_num)
+
+
+log_transformer = FunctionTransformer(np.log, inverse_func=np.exp)
+log_pop = log_transformer.transform(housing[['population']])
+
+
+num_pipeline = Pipeline([
+    ('impute', SimpleImputer(strategy='median')),
+    ('standardize', StandardScaler()),
+])
+
+housing_num_prepared = num_pipeline.fit_transform(housing_num)
+
+#numerical_attributes = ['longitude', 'latitude', 'housing_median_age', 'total_rooms',
+#'total_bedrooms', 'population', 'households', 'median_income']
+#categorial_attributes = ['ocean_proximity']
+
+cat_pipeline = make_pipeline(
+   SimpleImputer(strategy='most_frequent'),
+   OneHotEncoder(handle_unknown='ignore')
+)
+
+#preprocessing = ColumnTransformer(
+#    [
+#        ('num', num_pipeline, numerical_attributes),
+#        ('cat', cat_pipeline, categorial_attributes),
+#    ]
+#)
+
+#housing_prepared = preprocessing.fit_transform(housing)
+
+
+def column_ratio(X):
+    return X[:, [0]] / X[:, [1]]
+
+def ratio_name(function_transformer, feature_names_in):
+    return ["ratio"]
+
+def ratio_pipeline():
+    return make_pipeline(
+        SimpleImputer(strategy='median'),
+        FunctionTransformer(column_ratio, feature_names_out=ratio_name),
+        StandardScaler())
+
+log_pipeline = make_pipeline(
+    SimpleImputer(strategy='median'),
+    FunctionTransformer(np.log, feature_names_out='one-to-one'),
+    StandardScaler())
+
+cluster_simil = ClusterSimilarity(n_clusters=10, gamma = 1., random_state=42)
+
+default_num_pipeline = make_pipeline(SimpleImputer(strategy='median'), 
+                                     StandardScaler())
+
+
+preprocessing = ColumnTransformer([
+    ('bedrooms', ratio_pipeline(), ['total_bedrooms', 'total_rooms']),
+    ('rooms_per_house', ratio_pipeline(), ['total_rooms', 'households']),
+    ('people_per_house', ratio_pipeline(), ['population', 'households']),
+    ('log', log_pipeline, ['total_bedrooms', 'total_rooms', 'population', 'households', 'median_income']),
+    ('geo', cluster_simil, ['latitude', 'longitude']),
+    ('cat', cat_pipeline, make_column_selector(dtype_include=object)),
+],
+remainder=default_num_pipeline) #===== ONE COLUMN REMAINING: HOUSING_MEDIAN_AGE
+
+housing_prepared = preprocessing.fit_transform(housing)
+
+lin_reg = make_pipeline(preprocessing, LinearRegression())
+
+lin_reg.fit(housing, housing_labels)
+
+housing_predictions = lin_reg.predict(housing)
+
+lin_rmse = root_mean_squared_error(housing_labels, housing_predictions)
+
+tree_reg = make_pipeline(preprocessing, DecisionTreeRegressor(random_state=42))
+tree_reg.fit(housing, housing_labels)
+
+
